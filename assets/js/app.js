f1168bbf33816edceff2f32f4ddd329df5d21bcf
victory <makemvictorine@allotraining.com>
2019-11-15 16:42:45 +0100

    mise a jour

---
+++ assets/js/app.js
@@ -0,0 +1,44 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you require will output into a single css file (app.css in this case)
+require('../css/app.css');
+var $ = require('jquery');
+require('popper.js');
+require('bootstrap');
+require('bootstrap/dist/js/bootstrap.bundle');
+require('../dist/css/adminlte.min.css');
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
+require('../dist/css/adminlte.min.css');
+require('../icheck/skins/flat/blue.css');
+require('../morris/morris.css');
+require('../jvector/jvector/jquery-jvectormap.css');
+require('../datepicker/dist/css/bootstrap-datepicker3.css');
+require('../daterangepicker/daterangepicker.css');
+require('../bootstrap-wysihtml5-0.0.2/bootstrap-wysihtml5-0.0.2.css');
+require('../plugins/jquery/jquery.min.js');
+require('../plugins/bootstrap/js/bootstrap.bundle.min.js');
+require('../morris/morris.min.js');
+require('../jquery.sparkline/dist/jquery.sparkline.min.js');
+require('../jvector/jvector/jquery-jvectormap.js');
+require('../jvector/jvector/tests/assets/jquery-jvectormap-world-mill-en.js');
+require('../jQuery-Knob/js/jquery.knob.js');
+require('../daterangepicker/daterangepicker.js');
+require('../datepicker/dist/js/bootstrap-datepicker.js');
+require('../bootstrap-wysihtml5-0.0.2/bootstrap-wysihtml5-0.0.2.min.js');
+require('../jQuery-slimScroll/jquery.slimscroll.min.js');
+require('../plugins/fastclick/fastclick.js');
+require('../dist/js/adminlte.js');
+require('../dist/js/pages/dashboard.js');
+require('../dist/js/demo.js');
+
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+// const $ = require('jquery');
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
